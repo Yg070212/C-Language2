9b8d1f1b65ca16edce7e1ce4ebd176e1aef872d7
Yg070212 <winter070212@gmail.com>
2024-12-12 19:46:25 +0900

    24-12-12 <random 함수>

--- Program/Program.c
+++ Program/Program.c
@@ -16,6 +16,15 @@ void Shuffle(int array [], int size)
 	}
 }
 
+void View(int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf("♥");
+	}
+	printf("\n");
+}
+
 int main()
 {
 #pragma region rand() 함수
@@ -31,17 +40,93 @@ int main()
 
 #pragma region 셔플 함수
 
+	// srand(time(NULL));
+	// 
+	// int list[SIZE] = { 1, 2, 3, 4, 5 };
+	// 
+	// Shuffle(list, SIZE);
+	// 
+	// for (int i = 0; i < SIZE; i++)
+	// {
+	// 	printf("%d", list[i]);
+	// }
+#pragma endregion
+
+#pragma region localtime() 함수
+	// time_t currentTime = 0;
+	// 
+	// time(&currentTime);
+	// 
+	// struct tm* timePointer = localtime(&currentTime);
+	// 
+	// printf("%d 년\n", timePointer->tm_year+1900);
+	// printf("%d 월\n", timePointer->tm_mon+1);
+	// printf("%d 일\n", timePointer->tm_mday);
+	// printf("%d 요일\n", timePointer->tm_wday);
+	// 
+	// switch (timePointer->tm_wday)
+	// {
+	// case 0: printf("일요일\n", timePointer->tm_wday);
+	// 	break;
+	// case 1: printf("월요일\n", timePointer->tm_wday);
+	// 	break;
+	// case 2: printf("화요일\n", timePointer->tm_wday);
+	// 	break;
+	// case 3: printf("수요일\n", timePointer->tm_wday);
+	// 	break;
+	// case 4: printf("목요일\n", timePointer->tm_wday);
+	// 	break;
+	// case 5: printf("금요일\n", timePointer->tm_wday);
+	// 	break;
+	// case 6: printf("토요일\n", timePointer->tm_wday);
+	// 	break;
+	// }
+	// 
+	// printf("%d 시\n", timePointer->tm_hour);
+	// printf("%d 분\n", timePointer->tm_min);
+	// printf("%d 초\n", timePointer->tm_sec);
+#pragma endregion
+
+#pragma region UP - DOWN Game
+	
 	srand(time(NULL));
 
-	int list[SIZE] = { 1, 2, 3, 4, 5 };
+	int Health = 5;
+	int answer = 0;
+	int computer = rand() % 50 + 1;
 
-	Shuffle(list, SIZE);
+	printf(" > Game Start < \n\n");
 
-	for (int i = 0; i < SIZE; i++)
+	while (Health > 0)
 	{
-		printf("%d", list[i]);
+		printf("체력 : ");
+		View(Health);
+
+		printf("player : ");
+		scanf_s("%d", &answer);
+
+		if (answer == computer)
+		{
+			printf("\nV I C T O R Y\n");
+			break;
+		}
+		else if (answer < computer)
+		{
+			printf("\ncomputer가 가지고 있는 값 보다 적습니다.\n");
+			Health --;
+		}
+		else if (answer > computer)
+		{
+			printf("\ncomputer가 가지고 있는 값 보다 큽니다.\n");
+			Health--;
+		}
+	}
+	if (Health <= 0)
+	{
+		printf("\nD E F E A T\n");
 	}
 #pragma endregion
+
 
 	return 0;
 }
