860c432e4e7a7b64486e1102966cd5da6238afc1
Yg070212 <winter070212@gmail.com>
2024-12-19 19:46:22 +0900

    24-12-19 <파일 입출력>

--- Program/Program.c
+++ Program/Program.c
@@ -1,77 +1,51 @@
 #include <stdio.h>
 
-#define WITDH 10
-#define HEIGHT 10
+#define SIZE 10000
 
-char maze[WITDH][HEIGHT] =
+void Load(const char * fileName)
 {
-	// 0 = 공백
-	// 1 = ■
-	// 2 = 도착 포인트
+	// "r" read : 파일을 읽기위해 사용하는 모드입니다.
+	FILE* file = fopen(fileName, "r");
 
-	{'1', '1', '1', '1', '1', '1', '1', '1', '1', '1'},
-	{'1', '0', '1', '1', '1', '0', '1', '1', '1', '1'},
-	{'1', '0', '1', '1', '1', '0', '1', '1', '1', '1'},
-	{'1', '0', '1', '0', '0', '0', '0', '0', '1', '1'},
-	{'1', '0', '1', '0', '1', '1', '1', '0', '1', '1'},
-	{'1', '0', '1', '0', '1', '0', '1', '0', '0', '1'},
-	{'1', '0', '1', '0', '1', '0', '1', '1', '0', '1'},
-	{'1', '0', '1', '0', '1', '0', '1', '1', '0', '1'},
-	{'1', '0', '0', '0', '0', '0', '1', '1', '2', '1'},
-	{'1', '1', '1', '1', '1', '1', '1', '1', '1', '1'},
-};
+	char buffer[SIZE] = { 0, };
 
-void Render()
-{
-	for (int i = 0; i < WITDH; i++)
-	{
-		for (int j = 0; j < HEIGHT; j++)
-		{
-			printf("%c", maze[i][j]);
-			if ('0')
-			{
-				printf("  ", maze[i][j]);
-			}
-			if ('1')
-			{
-				printf("■", maze[i][j]);
-			}
-			if ('2')
-			{
-				printf("★", maze[i][j]);
-			}
-		}
-		printf("\n");
-	}
+	// 첫 번째 매개변수 : 읽은 데이터를 저장할 메모리 버퍼의 포인터
+	// 두 번째 매개변수 : 각 데이터 항목의 크기
+	// 세 번째 매개변수 : 데이터를 읽어올 데이터 항목의 수
+	// 네 번째 매개변수 : 데이터를 읽어올 파일의 포인터 변수
+
+	fread(buffer, 1, SIZE, file);
+
+	printf("%s", buffer);
+
+	fclose(file);
+
 }
 
 int main()
 {
-#pragma region (2)차원 배열
-	// 배열의 요소로 또 다른 배열을 가리는 배열입니다.
+#pragma region 파일 쓰기
 
-	// int array2D[3][3] =
-	// {
-	// 	{1,2,3},
-	// 	{4,5,6},
-	// 	{7,8,9},
-	// };
+	// "w" write : 파일 쓰기 모드로 진행합니다.
+	// FILE* file = fopen("data.txt", "w");
 	// 
-	// for (int i = 0; i < 3; i++)
-	// {
-	// 	for (int j = 0; j < 3; j++)
-	// 	{
-	// 		printf("%d ", array2D[i][j]);
-	// 	}
-	// 	printf("\n");
-	// }
+	// fputs("Character Name\n", file);
+	// fputs("Healthd\n", file);
+	// fputs("Level\n", file);
 
-	// 2차원 배열은 행과 열로 구분되며 앞에 있는 배열은
-	// 열을 의미하고, 뒤에 있는 배열은 행을 의미합니다.
+	// Health
+	// Level
+
+	// fclose(file);
 
 #pragma endregion
 
-	Render();
+#pragma region 파일 읽기
+
+	Load("Unit.txt");
+
+#pragma endregion
+
 
 	return 0;
 }
